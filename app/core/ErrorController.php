d22ba50da87bac98000e8f84d6d2b9a028ec69b7
gorguimarena <gorguimarena@gmail.com>
2025-07-25 10:16:50 +0000

    project initializer

--- app/core/ErrorController.php
+++ app/core/ErrorController.php
@@ -6,5 +6,7 @@ use DevNoKage\Abstract\AbstractController;
 
 class ErrorController extends AbstractController
 {
-    public function _404(): void {}
+    public function _404(): void {
+        echo "404";
+    }
 }
--- routes/route.web.php
+++ routes/route.web.php
@@ -1,18 +1,17 @@
 <?php
 
-use AppDAF\CONTROLLER\CitoyenController;
-use AppDAF\CORE\ErrorController;
-use AppDAF\ENUM\KeyRoute;
+use DevNoKage\Enums\KeyRoute;
+use DevNoKage\ErrorController;
 
 $routes = [
+    '/' => [
+        KeyRoute::CONTROLLER->value => ErrorController::class,
+        KeyRoute::METHOD->value => '_404',
+        KeyRoute::MIDDLEWARE->value => []
+    ],
     '/404' => [
         KeyRoute::CONTROLLER->value => ErrorController::class,
-        KeyRoute::ACTION->value => '_404',
-        KeyRoute::MIDDLEWARES->value => []
-    ],
-    '/citoyen/{cni}' => [
-        KeyRoute::CONTROLLER->value => CitoyenController::class,
-        KeyRoute::ACTION->value => 'recherche',
-        KeyRoute::MIDDLEWARES->value => []
+        KeyRoute::METHOD->value => '_404',
+        KeyRoute::MIDDLEWARE->value => []
     ]
 ];
